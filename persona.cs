f49af465b27ee7fb89219d499d91d8f84b53a123
marcos6almonte <35741515+marcos6almonte@users.noreply.github.com>
2018-01-24 09:49:22 -0800

    Add files via upload

---
+++ persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraccion__interfaz_
+{
+    class persona
+    {
+        private string nombre { get; set; }
+        private int edad { get; set; }
+        private bool genero { get; set; }
+        
+
+        public persona(string nombre, int edad, bool genero)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.genero = genero;
+        }
+
+        public virtual string hablar()
+        {
+            return nombre +  ": ";
+        }
+        public virtual string Comportamiento()
+        {
+            return "Habilidad: ";
+        }
+       
+    }
+    
+}
