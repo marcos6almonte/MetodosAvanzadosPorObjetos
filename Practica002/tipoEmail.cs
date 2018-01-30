78a5fc5b04b6119245595550b125a682767c03e1
marcos6almonte <35741515+marcos6almonte@users.noreply.github.com>
2018-01-29 19:50:52 -0800

    Add files via upload

---
+++ Practica002/tipoEmail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180129_Practica02
+{
+    class tipoEmail: InterfaceIndividuo
+    {
+        public string nombre { get; set; }
+        public string email { get; set; }
+        public int edad { get; set; }
+        public string puesto { get; set; }
+        public string telefono { get; set; }
+
+        public bool validate()
+        {
+            if (email.Contains("@@"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
