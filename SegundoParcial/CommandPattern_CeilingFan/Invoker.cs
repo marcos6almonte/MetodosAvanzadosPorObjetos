11632225f9e335d544d17d3cf3a08e8a497f0df6
marcos6almonte <35741515+marcos6almonte@users.noreply.github.com>
2018-03-22 09:45:38 -0700

    Add files via upload

---
+++ SegundoParcial/CommandPattern_CeilingFan/Invoker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180320_Actividad_Command
+{
+    //Invoker
+    class Invoker
+    {
+        Command[] comandos = new Command[8];
+        public int n, indice = 0;
+
+        public void SetCommand(Command command)
+        {
+            comandos[indice] = command;
+            indice++;
+        }
+
+        public void ExecuteCommand(int n)
+        {
+            this.n = n;
+            for (int i=0;i<=n;i++)
+            {
+                Console.WriteLine("Execute mode");
+                comandos[i].Execute();
+            }
+            
+
+        }
+        public void UndoCommand()
+        {
+            Console.WriteLine("Undo command");
+            comandos[n].Undo();
+        }
+    }
+}
