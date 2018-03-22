4df46d340977ad243949fa8b749750c88dc10a21
marcos6almonte <35741515+marcos6almonte@users.noreply.github.com>
2018-03-22 09:50:42 -0700

    Add files via upload

---
+++ SegundoParcial/Actividad_CommandPattern_CeilingFan/CeilingFan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180320_Actividad_Command
+{
+    class CeilingFan
+    {
+        public void high()
+        {
+            Console.WriteLine("Speed: high");
+        }
+        public void medium()
+        {
+            Console.WriteLine("Speed: medium");
+        }
+        public void low()
+        {
+            Console.WriteLine("Speed: low");
+        }
+        public void off()
+        {
+            Console.WriteLine("Fan off");
+        }
+       
+       
+    }
+}
---
+++ SegundoParcial/Actividad_CommandPattern_CeilingFan/ConcreteFreeze.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180320_Actividad_Command
+{
+    class ConcreteFreeze: Command
+    {
+        public ConcreteFreeze(Fridge fridge, Lights lights, CeilingFan fan, int state) : base(fridge, lights, fan, state)
+        {
+
+        }
+
+        public override void Execute()
+        {
+            fridge.freeze();
+        }
+        public override void Undo()
+        {
+            fridge.unfreeze();
+        }
+
+    }
+}
