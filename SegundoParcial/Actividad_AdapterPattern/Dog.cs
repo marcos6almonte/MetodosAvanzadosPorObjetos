4b621d128225b29de5a2796e7aa6b7ecfb7859f9
marcos6almonte <35741515+marcos6almonte@users.noreply.github.com>
2018-04-03 20:37:39 -0700

    Add files via upload

---
+++ SegundoParcial/Actividad_AdapterPattern/Dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180403_AdapterPattern
+{
+    class Dog
+    {
+        public virtual void bark()
+        {
+            Console.WriteLine("Bark...");
+        }
+        public virtual void bite()
+        {
+            Console.WriteLine("Bite...");
+        }
+    }
+}
---
+++ SegundoParcial/Actividad_AdapterPattern/Wolf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _180403_AdapterPattern
+{
+    class Wolf
+    {
+        public virtual void howl()
+        {
+            Console.WriteLine("Howl...");
+        }
+        public virtual void run()
+        {
+            Console.WriteLine("Run...");
+        }
+    }
+}
